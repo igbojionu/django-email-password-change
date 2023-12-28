3fc9e24031eee4ecbfcc9663f4081df3ab2d3772
igbojionu <66530400+igbojionu@users.noreply.github.com>
2023-12-28 01:07:33 +0100

    Create models.py

---
+++ models.py
@@ -0,0 +1,6 @@
+# your_app/models.py
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
